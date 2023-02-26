d5183b074e0fa325a2c4326ba3c8464cbc5941e8
Adam Wachter <adam.wachter32@gmail.com>
2023-02-27 00:25:52 +0800

    Added movement to player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //player move speed
+    [SerializeField]
+    int speed = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,20 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        playerMovement();
+    }
+
+    //movement code
+    void playerMovement()
+    {
+        //Variables = input horizontal and vertical axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //Variable = new position (horizontal input, vertical input, 0)
+        Vector3 direction = new Vector3 (horizontalInput, verticalInput, 0);
+
+        //Movement (in this direction * at this speed * in real time)
+        transform.Translate (direction * speed * Time.deltaTime);
     }
 }
