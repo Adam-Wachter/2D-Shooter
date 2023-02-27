f37eec801bc7b1886eb98c0839f7b4d02daf9d4c
Adam Wachter <adam.wachter32@gmail.com>
2023-02-27 15:54:04 +0800

    Created player boundries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,6 +19,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         playerMovement();
+        playerBoundries();
     }
 
     //movement code
@@ -33,5 +34,43 @@ public class Player : MonoBehaviour
 
         //Movement (in this direction * at this speed * in real time)
         transform.Translate (direction * speed * Time.deltaTime);
+    }
+
+    //player boundries
+    void playerBoundries()
+    {
+        //preset boundries for x and y
+        float yBoundryBottom = -4;
+        float yBoundryTop = 5.9f;
+        float xBoundryLeft = -11.3f;
+        float xBoundryRight = 11.3f;
+
+        //If player position on the Y is less than -4
+        //Y = -4
+        if (transform.position.y <= yBoundryBottom)
+        {
+            transform.position = new Vector3(transform.position.x, yBoundryBottom, transform.position.z);
+        }
+
+        //If player position on the Y is greater than 5.9
+        //Y = 5.9
+        if (transform.position.y >= yBoundryTop)
+        {
+            transform.position = new Vector3(transform.position.x, yBoundryTop, transform.position.z);
+        }
+
+        //If player position on the X is less than -11.3
+        //set new position on X to 11.3
+        if (transform.position.x < xBoundryLeft)
+        {
+            transform.position = new Vector3(xBoundryRight, transform.position.y, transform.position.z);
+        }
+
+        //If player position on the X is greater than 11.3
+        //set new position on X to -11.3
+        if (transform.position.x > xBoundryRight)
+        {
+            transform.position = new Vector3(xBoundryLeft, transform.position.y, transform.position.z);
+        }
     }
 }
